56ef76287d2cf5fb576729d2f0ad1145c82a86e8
Hanumanthu <104497776+hanu7348@users.noreply.github.com>
2024-09-20 20:30:15 +0530

    Update models.py

--- videos/models.py
+++ videos/models.py
@@ -1,12 +1,52 @@
-# Step 3: Update models.py
 from django.db import models
-from storages.backends.s3boto3 import S3Boto3Storage
+from django.core.exceptions import ValidationError
+import os
+
+def validate_video_extension(value):
+    valid_extensions = ['.mp4', '.avi', '.mov', '.flv', '.wmv', '.webm', '.mkv']
+    ext = os.path.splitext(value.name)[1].lower()
+    if ext not in valid_extensions:
+        raise ValidationError(f'Unsupported file extension. Allowed extensions are: {", ".join(valid_extensions)}')
 
 class Video(models.Model):
-    title = models.CharField(max_length=100)
+    VIDEO_FORMATS = [
+        ('mp4', 'MP4'),
+        ('avi', 'AVI'),
+        ('mov', 'MOV'),
+        ('flv', 'FLV'),
+        ('wmv', 'WMV'),
+        ('webm', 'WebM'),
+        ('mkv', 'MKV'),
+    ]
+
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    file = models.FileField(storage=S3Boto3Storage())
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+    video_file = models.FileField(upload_to='videos/', validators=[validate_video_extension])
+    video_format = models.CharField(max_length=4, choices=VIDEO_FORMATS, editable=False)
+    view_count = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.video_file:
+            ext = os.path.splitext(self.video_file.name)[1][1:].lower()
+            format_dict = dict((k.lower(), k) for k, v in self.VIDEO_FORMATS)
+            if ext in format_dict:
+                self.video_format = format_dict[ext]
+            else:
+                raise ValidationError(f'Unsupported file extension. Allowed extensions are: {", ".join([f".{k}" for k in format_dict.keys()])}')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    video = models.ForeignKey(Video, related_name='comments', on_delete=models.CASCADE)
+    user = models.CharField(max_length=255)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user} on {self.video.title}'
